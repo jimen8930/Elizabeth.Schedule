428421cd7d59a5387a125ebad2ab612f5ae647e4
jimen8930 <128547615+jimen8930@users.noreply.github.com>
2023-04-30 21:10:56 -0400

    I added a function for time block and condition statements.

--- Assets/script.js
+++ Assets/script.js
@@ -12,24 +12,59 @@ $(function () {
   $(".saveBtn").click("onClick",function() {
     console.log("I am clicked.");
     localStorage.setItem('time', 'text');
-    console.log("I am storing.")
+    console.log("I am storing.");
+    localStorage.getItem('text');
+    alert("It's retrieved.")
 
+  //Do I need a form?
 
 
   })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
+  // past, present, and future classes? 
+  
+  // Get the current hour
+let currentHour = dayjs().hour();
+
+// Loop through each time block
+$(".time-block").each(function() {
+  // Get the hour from the time block id
+  let blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+  // Compare the block hour to the current hour and apply class accordingly
+  //Here are the conditions for the hour blocks to change
+  if (blockHour < currentHour) {
+    $(this).addClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("future");
+
+  } else if (blockHour === currentHour) {
+    $(this).addClass("present");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+
+  } else {
+    $(this).addClass("future");
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+  }
+});
+
+
+  //How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  const value = localStorage.getItem('text');
+  // attribute of each time-block be used to do this? Need Help
+
+  
   
   // TODO: Add code to display the current date in the header of the page.
-  const now = dayjs('dddd, MMM DD YYYY, h:mm:ss');
+  //I need help with changing timezone. 
+  const now = dayjs();
 $("#currentDay").text(now);
-    
+ 
 });
