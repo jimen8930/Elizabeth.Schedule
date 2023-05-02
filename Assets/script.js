782e043b16308b5beedecd6ef0755344ccb6718e
jimen8930 <128547615+jimen8930@users.noreply.github.com>
2023-05-01 23:23:07 -0400

    Added get item code in the js file and added more time blocks for html file

--- Assets/script.js
+++ Assets/script.js
@@ -1,70 +1,70 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 //This is similar to $(document).ready(function) which prevents JQuery from loading before
 //the entire document finish loading.
+$(document).ready(function(){
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $(".saveBtn").click("onClick",function() {
-    console.log("I am clicked.");
-    localStorage.setItem('time', 'text');
-    console.log("I am storing.");
-    localStorage.getItem('text');
-    alert("It's retrieved.")
-
-  //Do I need a form?
-
-
+  $(".saveBtn").on("click", function () {
+    //Step 1 get data from text box.
+    //Step 2 save data to the local storage. 
+    var values = $(this).siblings('.description').val()
+var time = $(this).parent().attr('id')
+    localStorage.setItem(time, values)
   })
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? 
   
   // Get the current hour
-let currentHour = dayjs().hour();
+  let currentHour = dayjs().hour();
 
-// Loop through each time block
-$(".time-block").each(function() {
-  // Get the hour from the time block id
-  let blockHour = parseInt($(this).attr("id").split("hour")[1]);
+  // Loop through each time block
+  $(".time-block").each(function () {
+    // Get the hour from the time block id
+    let blockHour = parseInt($(this).attr("id").split("-")[1]);
+    console.log(blockHour)
+    // Compare the block hour to the current hour and apply class accordingly
+    //Here are the conditions for the hour blocks to change
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+      $(this).removeClass("present");
+      $(this).removeClass("future");
 
-  // Compare the block hour to the current hour and apply class accordingly
-  //Here are the conditions for the hour blocks to change
-  if (blockHour < currentHour) {
-    $(this).addClass("past");
-    $(this).removeClass("present");
-    $(this).removeClass("future");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+      $(this).removeClass("past");
+      $(this).removeClass("future");
 
-  } else if (blockHour === currentHour) {
-    $(this).addClass("present");
-    $(this).removeClass("past");
-    $(this).removeClass("future");
-
-  } else {
-    $(this).addClass("future");
-    $(this).removeClass("past");
-    $(this).removeClass("present");
-  }
-});
+    } else {
+      $(this).addClass("future");
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+    }
+  });
 
 
   //How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this? Need Help
 
-  
-  
+
+
   // TODO: Add code to display the current date in the header of the page.
   //I need help with changing timezone. 
   const now = dayjs();
-$("#currentDay").text(now);
- 
+  $("#currentDay").text(now);
+
+  //Get from local storage
+  $('#hour-8 .description').val(localStorage.getItem("hour-8"))
+  $('#hour-9 .description').val(localStorage.getItem("hour-9"))
+  $('#hour-10 .description').val(localStorage.getItem("hour-10"))
+  $('#hour-11 .description').val(localStorage.getItem("hour-11"))
+  $('#hour-12 .description').val(localStorage.getItem("hour-12"))
+  $('#hour-13 .description').val(localStorage.getItem("hour-13"))
+  $('#hour-14 .description').val(localStorage.getItem("hour-14"))
+  $('#hour-15 .description').val(localStorage.getItem("hour-15"))
+  $('#hour-16 .description').val(localStorage.getItem("hour-16"))
+  $('#hour-17 .description').val(localStorage.getItem("hour-17"))
 });
+})
